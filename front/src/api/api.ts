274ff86de9c827cca139e0508fd55b41f1959e07
Malza0408 <parkjh04080@gmail.com>
2022-08-17 11:25:19 +0900

    [Add] get api

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -2,6 +2,23 @@ import { Response } from "../common/types/interface";
 
 const baseUrl = String(process.env.REACT_APP_SERVER_URL);
 
+const get = async <T extends Response>(endpoint: string) => {
+  const url = baseUrl + endpoint;
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  const result: T = await res.json();
+
+  if (result.statusCode >= 400) {
+    alert(result.message);
+    throw new Error(result.error);
+  }
+
+  return result;
+};
+
 const post = async <T extends Response, U>(endpoint: string, data: U) => {
   const url = baseUrl + endpoint;
 
@@ -9,17 +26,18 @@ const post = async <T extends Response, U>(endpoint: string, data: U) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
     body: JSON.stringify(data),
   });
-
   const result: T = await res.json();
+
   if (result.statusCode >= 400) {
     alert(result.message);
-    throw new Error(result.message);
+    throw new Error(result.error);
   }
 
   return result;
 };
 
-export { post };
+export { post, get };
