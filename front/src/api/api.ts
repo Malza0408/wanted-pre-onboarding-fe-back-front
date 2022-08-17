f7e4c30812756a0c573b5a2eb3b3bebcdbe7b5c9
Malza0408 <parkjh04080@gmail.com>
2022-08-17 21:35:58 +0900

    [Add] post, delete api

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -21,7 +21,6 @@ const get = async <T extends Response>(endpoint: string) => {
 
 const post = async <T extends Response, U>(endpoint: string, data: U) => {
   const url = baseUrl + endpoint;
-
   const res = await fetch(url, {
     method: "POST",
     headers: {
@@ -40,4 +39,38 @@ const post = async <T extends Response, U>(endpoint: string, data: U) => {
   return result;
 };
 
-export { post, get };
+const put = async <T extends Response, U>(endpoint: string, data: U) => {
+  const url = baseUrl + endpoint;
+  const res = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(data),
+  });
+  const result: T = await res.json();
+
+  if (result.statusCode >= 400) {
+    alert(result.message);
+    throw new Error(result.error);
+  }
+
+  return result;
+};
+
+const del = async (endpoint: string) => {
+  const url = baseUrl + endpoint;
+  const res = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  if (res.status >= 400) {
+    alert("요청이 실패 했습니다!");
+  }
+  console.log(res);
+};
+
+export { post, get, put, del as delete };
